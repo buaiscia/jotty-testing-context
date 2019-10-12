9cd6f72cbb53c3271c41bcc8e1356e9b7061634a
silvanaweb <silvanasono@yahoo.it>
2019-10-12 18:20:14 +0100

    Guess words with guessed words prop

--- src/components/GuessWords.js
+++ src/components/GuessWords.js
@@ -4,6 +4,27 @@ const GuessWords = props => {
   let content;
   if (props.guessedWords.length === 0) {
     content = <span data-test="guess-instructions">Guess a word!</span>;
+  } else {
+    const guessedWordsMap = props.guessedWords.map((word, index) => (
+      <tr key={index} data-test="guessed-word">
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    content = (
+      <div data-test="guessed-words">
+        <h3>Guessed words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsMap}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{content}</div>;
 };
--- src/components/GuessWords.test.js
+++ src/components/GuessWords.test.js
@@ -22,3 +22,27 @@ describe("if there are no words guessed", () => {
     expect(component.text().length).not.toBe(0);
   });
 });
+
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 3 }
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders guess words section", () => {
+    const node = findByTestAttr(wrapper, "guessed-words");
+    expect(node.length).toBe(1);
+  });
+  test("displays correct number of guess words", () => {
+    const nodes = findByTestAttr(wrapper, "guessed-word");
+    expect(nodes.length).toBe(guessedWords.length);
+  });
+});
