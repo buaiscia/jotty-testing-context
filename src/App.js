73641b10c05221c6fb3e4a8dbe1a86337e267e75
silvanaweb <silvanasono@yahoo.it>
2019-10-15 19:53:23 +0100

    add useReducer to App to manage multiple state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Congrats from "./components/Congrats";
 import GuessWords from "./components/GuessWords";
+import hookActions from "./hooks/hookActions";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container" data-test="component-app">
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
-      </div>
-    );
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type ${action.type}`);
   }
+};
+function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = secretWord =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+  return <div data-test="component-app"></div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "./App";
 import { findByTestAttr } from "./test/utils";
+import hookActions from "./hooks/hookActions";
 
+const mockGetSecretWord = jest.fn();
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+  // use mount because useEffect is not called with shallow
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
 it("App renders without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord called", () => {
+  it("secretWord called on mount", () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+  it("secretWord not updated on app update", () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
 });
